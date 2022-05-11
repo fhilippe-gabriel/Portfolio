0d5b0f7c28e4fe3299fde06d12ef2df7bee7314c
fhilippe-gabriel <fhilippedev@gmail.com>
2022-05-10 23:00:40 -0300

    :up: Atualizando elementos

--- src/App.js
+++ src/App.js
@@ -123,13 +123,13 @@ function App() {
               <p className="mt-6 text-base text-2x text-gray-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Só vc mesmo
               </p>
-              {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
                     href="#"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Get started
+                    😘
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -137,10 +137,10 @@ function App() {
                     href="#"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
-                    Live demo
+                    😘😘😘
                   </a>
                 </div>
-              </div> */}
+              </div>
             </div>
           </main>
         </div >
